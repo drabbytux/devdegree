3b2682a964855fe518bb9c7241a425b6a4baeb69
David Little <drabbytux@gmail.com>
2020-10-16 07:02:46 -0400

    Adding vehical methods

---
+++ vehicle_methods.rb
@@ -0,0 +1,19 @@
+# Test Drive
+#
+# David Little
+# Oct 14 2020
+
+def accelerate
+    puts "Stepping on the gas"
+    puts "Speeding up"
+end
+
+def sound_horn
+  puts "Pressing the horn button"
+  puts "Beep beep!"
+end
+
+def use_headlights(brightness = "low-beam")
+  puts "Turning on #{brightness} headlights"
+  puts "Watch out for deer!"
+end
